d4ce2d45b87dbd238f0f1cd4634fb5c90dce19d0
Usuario <Usuario@CPD02-M10>
2024-03-05 11:43:34 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleTrabalho2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTrabalho2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o salário fixo: ");
+            double fixo = double.Parse(Console.ReadLine());
+            Console.Write("Informe a quantidade de peças vendidas: ");
+            int qtde = int.Parse(Console.ReadLine());
+            Console.Write("Informe o valor da comissão por peça: ");
+            double comissao = double.Parse(Console.ReadLine());
+            Console.Write("Informe a kilometragem percorrida: ");
+            int km = int.Parse(Console.ReadLine());
+            Console.Write("Informe o valor por km percorrido: ");
+            double valor_km=double.Parse(Console.ReadLine());
+            double salario_final = fixo + (qtde*comissao) +(km * valor_km);
+            Console.WriteLine("Salário final do vendedor: " + salario_final.ToString("C"));
+        }
+    }
+}
